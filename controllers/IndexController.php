52af47d315a8f2258401ecbc088becaebbe469e9
Vladislav <vla97657234@gmail.com>
2019-03-06 19:27:44 +0800

    Complete adding HTML structure and CSS.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -10,5 +10,7 @@
 function indexAction($smarty){
     $smarty->assign('pageTitle', 'Главная страница');
 
+    loadTemplate($smarty, 'header');
     loadTemplate($smarty, 'index');
+    loadTemplate($smarty, 'footer');
 }
